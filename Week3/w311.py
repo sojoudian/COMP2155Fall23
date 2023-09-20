5e757c3c8408fac856a704380906e95b9e50510c
Maziar Sojoudian <maziar@Nagini.local>
2023-09-20 13:11:58 -0400

    read data from a CSV file using csv module in Python

--- Week3/w311.py
+++ Week3/w311.py
@@ -1,5 +1,12 @@
 #working woth CSV files in Python
 
-CSVfile = open("file.csv", "r")
-a=CSVfile.read()
-print(a)
+# CSVfile = open("file.csv", "r")
+# a=CSVfile.read()
+# print(a)
+
+import csv
+with open("file.csv", "r+") as f:
+    data = csv.reader(f)
+    for row in data:
+        print(row, end="\n")
+    f.close()
