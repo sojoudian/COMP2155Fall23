4667c660ac389e7b0842d1ba730cca061f77ea41
Maziar Sojoudian <maziar@Nagini.local>
2023-09-20 13:04:41 -0400

    add remove directory using python

--- Week3/w310.py
+++ Week3/w310.py
@@ -5,3 +5,7 @@ os.chdir("/Users/maziar/py/COMP2155/Week3/testDir")
 os.remove("/Users/maziar/py/COMP2155/Week3/testDir/basedOnW39.txt")
 os.chdir("/Users/maziar/py/COMP2155/Week3/")
 os.rmdir("/Users/maziar/py/COMP2155/Week3/testDir")
+
+#r"C:\new" \n =>
+# c: 
+# ew
