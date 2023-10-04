95e7446ce86c9dbe65bf9a5799b958901e8692cd
Maziar Sojoudian <maziar@sojoudian.net>
2023-10-04 13:07:47 -0400

    cisco ssh latest

--- week5/cisco.py
+++ week5/cisco.py
@@ -6,14 +6,18 @@ sshClient = paramiko.SSHClient()
 #to fix known issues with SSH we have to set the policy to auto add the keys
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-sshClient.connect(  hostname='192.168.25.129',username='albus',password='Dumbledore',port=22)
+sshClient.connect(  hostname='192.168.122.100',username='u1',password='cisco',port=22)
 
 isSSHActive = sshClient.get_transport().is_active()
 print(isSSHActive)
 print("Successfully connected to the host")
 
 shell = sshClient.invoke_shell()
-shell.send("ls -l\n")
+shell.send("enable\n")
+time.sleep(1)
+shell.send("cisco\n")
+time.sleep(1)
+shell.send("show version\n")
 time.sleep(1)
 respone = shell.recv(1000) #receive 5000 bytes
 output = respone.decode('utf-8')
